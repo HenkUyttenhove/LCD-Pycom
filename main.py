7fe5fb6fa92335cf03dcc7bf9192663695839301
HenkUyttenhove <henk.uyttenhovedev@kawanda.be>
2021-11-09 23:04:33 +0100

    Please accept examples

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ import socket     # import the IP stack
 import struct
 import time       # required to support the delay
 from LCD import CharLCD
+import utime
 #Pin.setmode(Pin.BCM)
 
 lcd = CharLCD()
 lcd.message('Hello', 2)
 lcd.set_line(1)
 lcd.message('World!', 2)
-time.sleep(3.0)
+time.sleep(5.0)
+lcd.clear()
+lcd.set_cursor(0, 0)
+lcd.message("The year:", 0)
+lcd.set_cursor(10, 0)
+lcd.message(str(utime.localtime()[0]), 0)
+lcd.set_line(1)
+lcd.message("happy newyear", 0)
